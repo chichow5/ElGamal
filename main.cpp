4f55ad4b30271abad88e10bc659674509c37b35f
chichow5 <79318454+chichow5@users.noreply.github.com>
2021-11-18 18:59:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -58,6 +58,9 @@ namespace ElGamal {
         putchar('\n');
     }
 
+    /**
+     * 打印菜单，用于用户选择密钥长度
+     */
     void menu(){
         printf(""
 				" _____   _    ____                               _ \n"
@@ -97,19 +100,25 @@ namespace ElGamal {
         }
         key_size = n;
     }
+
+    /**
+     * 初始工作
+     * 初始化大整数对象和符号逆变换
+     */
     void init(){
         mpz_init(q);mpz_init(p);mpz_init(alpha);
         mpz_init(d);mpz_init(beta);mpz_init(k);
         mpz_init(p);mpz_init(p_1);mpz_init(t1);
         mpz_init(t2);mpz_init(c1);
-        gmp_randinit_default(state);
+        //gmp_randinit_default(state);
         //用于随机数生成
-        //gmp_randinit_mt(state);
+        gmp_randinit_mt(state);
         //根据系统时间设置random的seed
-        //gmp_randseed_ui(state,(unsigned int)(time(NULL)));
+        gmp_randseed_ui(state,(unsigned int)(time(NULL)));
         for (int i=0; i<MSGLEN; i++){
             mpz_init2(c2[i], key_size);
         }
+        // 字符到数值逆转换
         f1['1'] = 1;  f1['2'] = 2;
         f1['3'] = 3;  f1['4'] = 4;
         f1['5'] = 5;  f1['6'] = 6;
@@ -122,6 +131,11 @@ namespace ElGamal {
         f1['E'] = 14; f1['e'] = 14;
         f1['F'] = 15; f1['f'] = 15;
     }
+
+    /**
+     * 生成密匙
+     * 主要是p、alpha、d、beta
+     */
     void keyGen(){
         while(true) {
             //生成key_size位的随机数
@@ -131,7 +145,10 @@ namespace ElGamal {
             // p-1因子只有1，2，q，p-1四个
             mpz_mul_ui(p, q, 2);
             mpz_add_ui(p, p, 1);
-            //使用GMP内置的Miller-Rabin测试p是否素数
+            // 使用GMP内置的Miller-Rabin测试p是否素数
+            // 当key_size过大时，程序将会花费大量时间判断
+            // 生成的p是否素数
+            // 简易使用 key_size < 1024的情况
             if (mpz_probab_prime_p(p,15)>0) break;
         }
         gmp_printf("p :     %Zd\n", p);
@@ -145,7 +162,7 @@ namespace ElGamal {
             mpz_powm_ui(t1, alpha, 2, p);//t1 = alpha^2 % p
             mpz_powm(t2, alpha, q, p);   //t2 = alpha^q % p
             if (mpz_cmp_ui(t1, 1) != 0 && mpz_cmp_ui(t2, 1) != 0){
-                // g^2 % p != 1 并且 g^q % p != 1
+                // g^2 % p != 1 and g^q % p != 1
                 break;
             }
         }
@@ -157,15 +174,17 @@ namespace ElGamal {
             }
         }
         gmp_printf("d :     %Zd\n",d);
+        // beta = alpha^d % p
         mpz_powm(beta, alpha, d, p);
         gmp_printf("beta :  %Zd\n",beta);
     }
+
     /**
      * 将src按位拓展，只能分成2、4、8份
-     * @param src
-     * @param des
-     * @param len_src
-     * @param mul
+     * @param src 源字符串
+     * @param len_src 源字符串长度
+     * @param mul 扩长倍数
+     * @return 返回扩长后的字符指针（函数内部自行malloc）
      */
     unsigned char * strExpand(unsigned char* src, size_t len_src, int mul=2){
         static int b[]=  {0,0,4,  0,6,0,0,0,7};
@@ -181,10 +200,17 @@ namespace ElGamal {
         des[len_src*mul] = 0;
         return des;
     }
+
+    /**
+     * strExpand的逆变换
+     * @param src 源字符串
+     * @param len_src 源字符串长度
+     * @param mul 扩长倍数
+     * @return 返回扩长前的字符指针（函数内部自行malloc）
+     */
     unsigned char * strNarrow(unsigned char *src, size_t len_src, int mul=2){
         size_t malloc_size = (((len_src+mul-1)/mul)+1)*sizeof(char);
         auto des = (unsigned char *)malloc(malloc_size);
-        //memset(des, 0, malloc_size);
         int len = 0;
         des[0] = 0;
         for (int i=0; i<len_src; i++){
@@ -197,17 +223,32 @@ namespace ElGamal {
         des[++len] = 0;
         return des;
     }
+
+    /**
+     * 加密入口
+     * @param str 要加密的字符串
+     * @param len 字符串长度
+     */
     void encryption(char *str, size_t len){
-        while(true){
+        while(true){//生成 [1,p-1] 的随机数 k
             mpz_urandomm(k, state, p_1);
             if (mpz_cmp_ui(k, 0)!=0){//确保k大于0
                 break;
             }
         }
+        // c1 = alpha^k % p
         mpz_powm(c1, alpha, k, p);
         gmp_printf("c1 :    %Zd\n",c1);
+        // t1 = beta^k % p
         mpz_powm(t1, beta, k, p);
 
+        /* 出于性能和安全性考虑，需将特定长度字符串压缩为一个大整数
+         * 1）在ElGamal加密中，明文要求小于p
+         * 2）通过将字符串4个比特位化为一个16进制数，才能方便使用
+         *  函数 mpz_set_str 将数存入 gmp 大整数对象。这里近似
+         *  于将明文“分块”，每一块的大小是((key_size/8)-1)*2
+         *  （乘以2是因为明文通过strExpand扩展为两倍大小）
+         */
         auto tstr = strExpand((unsigned char*)str,len);
         int blocks = ((key_size/8)-1)*2;
         nc = 0;
@@ -220,11 +261,12 @@ namespace ElGamal {
                 ch = tstr[i+blocks];
                 tstr[i+blocks] = 0;
             }
+            //[tstr+i,tsrt+blocks)区间构成一个大整数
             mpz_set_str(c2[nc], reinterpret_cast<const char *>(tstr + i), 16);
-            //printf("<%s>\n",tstr+i);
             mpz_get_str((char *)s, 16, c2[nc]);
-            //printf("<%s>\n",(char*)s);
             gmp_printf("m[%d] :  %Zd\n",nc, c2[nc]);
+
+            //c2 = c2*t1 % p = m * beta^k % p
             mpz_mul(c2[nc], c2[nc], t1);
             mpz_mod(c2[nc], c2[nc], p);
             nc++;
@@ -232,6 +274,7 @@ namespace ElGamal {
                 tstr[i+blocks] = ch;
             }
         }
+        free(tstr);
         printf("====================================\n"
                "|do encryption                     |\n"
                "====================================\n");
@@ -239,36 +282,49 @@ namespace ElGamal {
             gmp_printf("c2[%d] : %Zd\n",i, c2[i]);
         }
     }
+
+    /**
+     * 解密函数
+     */
     void decryption(){
+        //存储最终的流结果
         std::stringstream sstream;
         // c1 = (c1^d)^-1 (mod p)
         mpz_powm(c1,c1,d,p);
-        mpz_invert(c1,c1,p);
+        mpz_invert(c1,c1,p);//求逆元
         printf("====================================\n"
                "|do decryption                     |\n"
                "====================================\n");
         for (int i=0; i<nc; i++){
+            //m = c2 * c1 % p = c2 * (c1^d)^-1 % p
             mpz_mul(c2[i],c2[i],c1);
             mpz_mod(c2[i],c2[i],p);
             gmp_printf("m[%d] :  %Zd\n",i,c2[i]);
             mpz_get_str((char*)s,16,c2[i]);
+            //字符串压缩：两位变一位（16进制）得到明文消息
             auto ss = strNarrow(s,strlen((char*)s));
             sstream<<ss;
-            //printf("%s\n",ss);
         }
         std::cout<<sstream.str()<<std::endl;
     }
-    void main(){
+
+    /**
+     * ElGamal入口
+     * @param src 要加密的字符串
+     * @param len 要加密的长度
+     * 程序自动生成随机数、密钥等
+     */
+    void main(char *src, int len){
         menu();
         init();
         keyGen();
-        char ss[] = "zhouzhiiiiiiiidiiiiiiziiiiioiiiiiiiipiiiiiiiiiiiiiiciiiik";
-        encryption(ss, strlen(ss));
+        encryption(src, len);
         decryption();
     }
 }
 
 int main() {
-    ElGamal::main();
+    char ss[] = "zhouzhiiiiiiiidiiiiiiziiiiioiiiiiiiipiiiiiiiiiiiiiiciiiik";
+    ElGamal::main(ss, strlen(ss));
     return 0;
 }
